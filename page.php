dc5d73b0bd412dab7bc4704141b553843ee442c2
nahian28 <nahiansylhet@gmail.com>
2023-03-12 12:54:55 +0600

    Updates

--- page.php
+++ page.php
@@ -1,8 +1,24 @@
 <?php get_header(); ?>
 
-	<main id="primary" class="site-main">
+<section class="breadcumb bg" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/img/breadcumb.jpg');">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 text-center">
+				<h4 class="breadcumb-title"><?php the_title();?></h4>
+				<ul class="breadcumb-nav">
+					<li class="breadcumb-list"><a href="<?php echo site_url();?>" class="breadcumb-link">Home</a></li>
+					<li class="breadcumb-list"><?php the_title();?></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
 
-		<?php
+	<main id="primary" class="site-main pt-5 pb-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+				<?php
 		while ( have_posts() ) :
 			the_post();
 
@@ -11,6 +27,11 @@
 		endwhile; // End of the loop.
 		?>
 
+				</div>
+			</div>
+		</div>
+
+		
 	</main><!-- #main -->
 
 <?php
